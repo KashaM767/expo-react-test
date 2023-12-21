590c69c94df90f473d642a6010bedc9626272049
Kasha Malloy <kashamalloy@gmail.com>
2023-12-21 17:26:48 +0000

    added links

--- App.js
+++ App.js
@@ -5,13 +5,23 @@ import SignUpScreen from './src/screens/SignupScreen/SignUpScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 
 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SignUpScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Signin">
+        <Tab.Screen name="signin" component={SigninScreen} />
+        <Tab.Screen name="signup" component={SignUpScreen} />
+        <Tab.Screen name="reset password" component={ForgotPasswordScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
